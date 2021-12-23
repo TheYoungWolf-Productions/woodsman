28f97d5d8818c1edfcef1d90124d6153a9a92ee0
TheYoungWolf-Productions <imafraz@hotmail.com>
2021-12-24 01:44:27 +0500

    Fix import statement

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
-import Sidebar from "../Sidebar/Sidebar";
+import Sidebar from "../../containers/Sidebar/Sidebar";
 import routes from "../../route.config";
 
 const Layout = () => {
